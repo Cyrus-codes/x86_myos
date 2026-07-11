9f707f3ebf3230855dacf0cfb73b8b8f57ce5dd0
Cyrus-codes <shivkakkar245@gmail.com>
2026-07-11 22:24:54 +0000

    Resolved merge conflicts

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 nasm -f bin boot.asm -o boot.bin
 nasm -f bin kernel.asm -o kernel.bin
 copy /b boot.bin+kernel.bin os-image.bin
-qemu-system-i386 -fda os-image.bin
+qemu-system-i386 -drive file=os-image.bin,format=raw,if=floppy
